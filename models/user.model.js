a6b5a17c346be33c437eb783a97abc1f6076bb30
Nikhil Gupta <nikhil062000@gmail.com>
2022-01-01 16:25:04 +0530

    login and register api funtions added

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+var crypto = require('crypto');
 
 const Schema = mongoose.Schema;
 
@@ -48,10 +49,24 @@ const userSchema = new Schema({
     password_hash: {
         type: String,
         required: true
-    }
+    },
+    salt: String
 }, {
     timestamps: true,
 });
+
+userSchema.methods.setPassword = function(password){
+    //creating a unique salt for a particular user
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    //hashing user's salt and password with 1000 iterations, 64 length and sha512 digest
+    this.password_hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+};
+
+userSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.password_hash == hash;
+}
 
 const User = mongoose.model('User', userSchema);
 
--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 let RSVP = require('../models/rsvp.model');
 
-router.route('/').get((req, res) => {
+router.route('/all').get((req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route('/register').post((req, res) => {
     const user_id = req.body.user_id;
     const name = req.body.name;
     const age = req.body.age;
@@ -25,5 +25,24 @@ router.route('/add').post((req, res) => {
         .then(() => res.json('User added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/login').post( async (req,res) => {
+    const user = await User.findOne({user_id : req.body.user_id});
+    if(user == null){
+        return res.status(400).send({message : "User not found"});
+    }
+    else {
+        if(user.validPassword(req.body.password)){
+            return res.status(201).send({
+                message: "User Logged In"
+            });
+        } 
+        else {
+            return res.status(400).send({
+                message: "wrong password"
+            });
+        }
+    }
+})
 
 module.exports = router;
